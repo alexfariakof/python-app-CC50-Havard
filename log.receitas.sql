f373dd2721489682e889aff5267b2e987bf1de06
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-05-04 13:04:36 -0300

    refatorando arquivos

--- log.receitas.sql
+++
@@ -1,9 +0,0 @@
--- sqlite3 DespesasPessoaisDB.db
--- .schema
-
-
-Select r.id, r.idUsuario, c.descricao as categoria, tc.descricao as tipo, strftime('%d/%m/%Y', r.data) as data, r.descricao, r.valor
-  From Receita r
- Inner Join Categoria c on r.idCategoria = c.id
- Inner Join TipoCategoria tc on c.idTipoCategoria = tc.id
- where r.idUsuario = 1 and c.idTipoCategoria = 2;
