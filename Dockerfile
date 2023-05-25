f494e677ae0aa41cfb3ac670fbf9b7a4d6981380
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2023-05-25 01:29:13 -0300

    dokerização ok
    Aplicação dockerizada e rodando no Azure

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,16 @@
 # For more information, please refer to https://aka.ms/vscode-docker-python
 FROM python:3.10-slim
 
+EXPOSE 80
 EXPOSE 5000
+EXPOSE 5002
+
+#Install locales  pt-br
+RUN apt-get update
+RUN apt-get install -y locales locales-all
+ENV LC_ALL pt_BR.UTF-8
+ENV LANG pt_BR.UTF-8
+ENV LANGUAGE pt_BR.UTF-8
 
 # Keeps Python from generating .pyc files in the container
 ENV PYTHONDONTWRITEBYTECODE=1
@@ -22,4 +31,4 @@ RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /
 USER appuser
 
 # During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
-CMD ["python", "-m", "flask", "run"]
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -56,18 +56,18 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return render_template("login.html")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return render_template("login.html")
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return render_template("login.html")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -575,4 +575,4 @@ def fillCategoriasPadrao(idUsuario):
     db.execute("INSERT INTO Categoria (idUsuario,idTipoCategoria ,descricao) VALUES  (?, 2, 'Benefício' );", idUsuario);
     db.execute("INSERT INTO Categoria (idUsuario,idTipoCategoria ,descricao) VALUES  (?, 2, 'Outro' );", idUsuario);
 
-app.run() 
+app.run(host="0.0.0.0")
